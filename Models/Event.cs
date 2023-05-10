f0bc347d5be0aa7c5853c155492ebda3c5abee50
lukaIvanic <luka.ivanic7@gmail.com>
2023-05-10 12:05:36 +0200

    connect to sql db and enabled CRUD

---
+++ Models/Event.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace EventusaBackend.Models
+{
+    [Table("events_table")]
+    public class Event
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using EventusaBackend.Models;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+var databaseName = "Server=tcp:eventusa-backend-test-server.database.windows.net,1433;Initial Catalog=eventusa-backend-test-database;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;User ID=eventusa-backend-test-admin;Password=pAss!#$%;";
+
+builder.Services.AddDbContext<EventContext>(options =>
+options.UseSqlServer( databaseName)
+);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
